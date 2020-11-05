2c7d785cd3ec81d1588e5beea1c5f0dd352c8f9a
David Edwards <Davidjames9610@gmail.com>
2020-11-05 18:16:41 +0200

    bootstrap added to form

--- src/components/RequestFormik.js
+++ src/components/RequestFormik.js
@@ -3,16 +3,11 @@ import moment from 'moment';
 
 import { Row, Col, Form } from 'react-bootstrap';
 import * as Yup from 'yup';
-// import FormControl from 'react-bootstrap/FormControl';
-// import FormGroup from 'react-bootstrap/FormGroup';
-// import FormLabel from 'react-bootstrap/FormLabel';
-
-// import { DateRangePicker } from "react-dates";
-// import "react-dates/initialize";
-// import "react-dates/lib/css/_datepicker.css";
+import FormControl from 'react-bootstrap/FormControl';
+import FormGroup from 'react-bootstrap/FormGroup';
+import FormLabel from 'react-bootstrap/FormLabel';
 
 import { useFormik } from 'formik';
-
 
 const SignupSchema = Yup.object().shape({
     firstName: Yup.string()
@@ -23,9 +18,15 @@ const SignupSchema = Yup.object().shape({
         .min(2, 'Too Short!')
         .max(50, 'Too Long!')
         .required('Required'),
-    email: Yup.string().email('Invalid email').required('Required'),
+    email: Yup.string()
+        .email('Invalid email')
+        .required('Required'),
+    reason: Yup.string()
+        .required('Required')
+        .min(2, 'Too Short!'),
+    type: Yup.string()
+        .required('A Type is Required')
 });
-
 
 const RequestFormik = () => {
     const formik = useFormik({
@@ -40,9 +41,8 @@ const RequestFormik = () => {
         validationSchema: SignupSchema,
     });
 
-
     return (
-        <form onSubmit={formik.handleSubmit}>
+        <form className="form__container" onSubmit={formik.handleSubmit}>
             <label htmlFor="firstName">First Name</label>
             <input
                 id="firstName"
@@ -68,6 +68,45 @@ const RequestFormik = () => {
                 onChange={formik.handleChange}
                 value={formik.values.email}
             />
+            <Row>
+                <Col>
+                    <FormGroup>
+                        <FormLabel>Reason</FormLabel>
+                        <p>error: {formik.errors.reason}</p>
+                        <FormControl
+                            as="textarea"
+                            rows={3}
+                            id="reason"
+                            name="reason"
+                            value={formik.values.reason}
+                            onChange={formik.handleChange}
+                        />
+                    </FormGroup>
+                </Col>
+                <Col>
+                    <FormGroup>
+                        <FormLabel>Example select</FormLabel>
+                        <p>error: {formik.errors.type}</p>
+                        <FormControl
+                            as="select"
+                            id="type"
+                            name="type"
+                            value={formik.values.type}
+                            onChange={formik.handleChange}
+                        >
+                            <option value="" label="Select a type" />
+                            <option value="Sick">Sick</option>
+                            <option value="Casual">Casual</option>
+                            <option value="Maternity">Maternity</option>
+                            <option value="Paternity">Paternity</option>
+                            <option value="Bereavement">Bereavement</option>
+                            <option value="Compensatory">Compensatory</option>
+                            <option value="Sabbatical">Sabbatical</option>
+                            <option value="Unpaid">Unpaid</option>
+                        </FormControl>
+                    </FormGroup>
+                </Col>
+            </Row>
             <button type="submit">Submit</button>
         </form>
     );
