1345c322fbc0ec736fc24a8544ea11637e39f59f
David Edwards <Davidjames9610@gmail.com>
2020-11-08 21:30:55 +0200

    heroku deployment

--- src/server/mySocket.js
+++ src/server/mySocket.js
@@ -1,9 +1,9 @@
 import io from 'socket.io-client';
 
 //[1] for local debuging and hosting (build mode too!)
-const mySocket = io.connect('http://localhost:8000');
+//const mySocket = io.connect('http://localhost:8000');
 
 //[2] for heroku
-//const mySocket = io.connect();
+const mySocket = io.connect();
 
 export { mySocket as default }
